f84a7abf291d8424115ff2d894d81b9ccf3e93f7
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2023-08-07 00:54:31 -0500

    Update Tramite_Electoral.ts

--- Model/Domino/Votacion/service/Tramite_Electoral.ts
+++ Model/Domino/Votacion/service/Tramite_Electoral.ts
@@ -1,46 +1,103 @@
 namespace Dominio.Votacion.service {
-    class TramiteElectoral {
-      /**
-       * Default constructor
-       */
-      constructor() {
-        // ...
+  class TramiteElectoral
+    implements VerificadorEstadoEleccion,
+    ContadorVotos,
+    VerificadorCandidato,
+    ValidadorVotante,
+    ConfirmadorVoto
+  {
+    private votos: Voto[]; // Array de votos almacenado en la clase.
+    private estadoEleccion: string; // Estado actual de la elección.
+
+    constructor(votos: Voto[], estadoEleccion: string) {
+      this.votos = votos;
+      this.estadoEleccion = estadoEleccion;
+    }
+
+    public verificacionEstadoEleccion(): string {
+      const estadoEleccion = this.estadoEleccion;
+      console.log(`Estado actual de la elección: ${estadoEleccion}`);
+      return estadoEleccion;
+    }
+
+    public contadorVotos(): number {
+      // Filtrar los votos confirmados
+      const votosConfirmados = this.votos.filter((voto) => voto.confirmado);
+
+      // Obtener el número total de votos confirmados
+      const totalVotos = votosConfirmados.length;
+
+      // Mostrar el resultado del conteo en la consola
+      console.log(`El número total de votos confirmados es: ${totalVotos}`);
+
+      return totalVotos;
+    }
+
+    public verificadorCandidato(idCandidato: number): boolean {
+      const candidatoEncontrado = this.candidatos.find(
+        (candidato) => candidato.idCandidato === idCandidato
+      );
+
+      if (!candidatoEncontrado) {
+        console.log(`Candidato con ID ${idCandidato} no encontrado.`);
+        return false;
       }
-  
-      /**
-       * Implement this method to check the election status.
-       */
-      public verificacionEstadoEleccion() {
-        // TODO implement here
+
+      const cumpleRequisitos = this.verificarRequisitos(candidatoEncontrado);
+
+      if (!cumpleRequisitos) {
+        console.log(
+          `Candidato con ID ${idCandidato} no cumple los requisitos para ser elegible.`
+        );
+        return false;
       }
-  
-      /**
-       * Implement this method to count votes.
-       */
-      public conteoVotos() {
-        // TODO implement here
+
+      console.log(`Candidato con ID ${idCandidato} es elegible.`);
+      return true;
+    }
+
+    // Método auxiliar para verificar los requisitos de elegibilidad del candidato.
+    private verificarRequisitos(candidato: Candidato): boolean {
+      // Supongamos que aquí se realiza una lógica más compleja para verificar los requisitos del candidato.
+      // Por ejemplo, podríamos verificar su edad, antecedentes, experiencia, etc.
+      // En esta implementación simulada, siempre devolvemos true.
+      return true;
+    }
+
+    public validacionVotante(idVotante: number): boolean {
+      const votanteValido = this.votantes.find(
+        (votante) => votante.idVotante === idVotante
+      );
+
+      if (!votanteValido) {
+        console.log(`Votante con ID ${idVotante} no encontrado o no es válido.`);
+        return false;
       }
-  
-      /**
-       * Implement this method to verify candidates.
-       */
-      public verificacionCandidato() {
-        // TODO implement here
+
+      console.log(
+        `Votante con ID ${idVotante} ha sido validado exitosamente y puede votar.`
+      );
+      return true;
+    }
+
+    public confirmacionVoto(idVoto: number): boolean {
+      const votoEncontrado = this.votos.find((voto) => voto.idVoto === idVoto);
+
+      if (!votoEncontrado) {
+        console.log(`Voto con ID ${idVoto} no encontrado.`);
+        return false;
       }
-  
-      /**
-       * Implement this method to validate voters.
-       */
-      public validacionVotante() {
-        // TODO implement here
+
+      if (votoEncontrado.confirmado) {
+        console.log(`Voto con ID ${idVoto} ya ha sido confirmado previamente.`);
+        return false;
       }
-  
-      /**
-       * Implement this method to confirm votes.
-       */
-      public confirmacionVoto() {
-        // TODO implement here
-      }
+
+      votoEncontrado.confirmado = true;
+      console.log(`Voto con ID ${idVoto} ha sido confirmado exitosamente.`);
+      return true;
     }
   }
+}
+
   
