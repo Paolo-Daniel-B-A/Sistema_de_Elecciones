d9699d9f798bf191154486ec2abd188d638b3704
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2023-08-07 00:23:42 -0500

    Update Eleccion.ts

--- Model/Domino/Votacion/entities/Eleccion.ts
+++ Model/Domino/Votacion/entities/Eleccion.ts
@@ -1,23 +1,31 @@
+//Principio de Responsabilidad Única (SRP - Single Responsibility Principle):
+//El SRP establece que una clase debe tener una sola responsabilidad y que esta debe estar encapsulada en esa clase.
+
+// Clase para representar el voto
 namespace Dominio.Votacion.entities {
-    class Eleccion {
-      private idEleccion: number;
-      private idVoto: number;
-      private nombre: string;
-      private fecha: string;
-      private estadoEleccion: string;
-  
-      /**
-       * Default constructor
-       */
-      constructor() {
-        // ...
-      }
-  
-      // Setter methods
-      // Implement setter methods here
-  
-      // Getter methods
-      // Implement getter methods here
+  class Voto {
+    private idVoto: number;
+    private idVotante: number;
+    private idCandidato: number;
+    private validado: boolean;
+
+    constructor(idVoto: number, idVotante: number, idCandidato: number, validado: boolean) {
+      this.idVoto = idVoto;
+      this.idVotante = idVotante;
+      this.idCandidato = idCandidato;
+      this.validado = validado;
+    }
+
+    // Getter methods
+    // Implement getter methods here
+  }
+}
+
+// Clase para operaciones con votos
+namespace Dominio.Votacion.services {
+  class ServicioVotos {
+    static encontrarCandidatoMasVotado(votos: Voto[]): number | null {
+      // Implementación para encontrar el candidato más votado (código previo)
     }
   }
-  
+}
